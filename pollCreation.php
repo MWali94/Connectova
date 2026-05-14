a4132653571fd43de5b5e9f41afe9dec1d694e77
Areesha0607 <Areeshasultan.07@gmail.com>
2026-05-14 14:08:12 +0500

    Updated Poll options

--- pollCreation.php
+++ pollCreation.php
@@ -12,15 +12,24 @@
 
     <form method="POST">
 
-        <input type="text" name="question" placeholder="Enter poll question">
+     <input type="text" name="question" placeholder="Enter poll question">
+     <br><br> 
+    
+     <div id="options-container">
 
         <input type="text" name="option1" placeholder="Option 1">
-
+     <br><br>
         <input type="text" name="option2" placeholder="Option 2">
-
+     <br><br>
         <input type="text" name="option3" placeholder="Option 3">
+     <br><br>
+        
+     </div>
 
-        <button type="submit">Create Poll</button>
+        <button type="button" onclick="addOption()">Add More Options</button>
+        <br><br>
+
+     <button type="submit">Create Poll</button>
 
     </form>
 
