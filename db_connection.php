b69fa6e7f3f6b455379c90dbaf0dd07fc07cecb9
warda501 <wardalatif3321@gmail.com>
2026-05-14 12:33:28 +0500

    DB file

---
+++ db_connection.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "connectova";
+
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+// Success message
+echo "Database connected successfully";
+
+?>
