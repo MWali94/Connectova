12cb09a5663679a02d162bb44a59814f81807a57
aminaghafoor <aminaghafoor250@gmail.com>
2026-05-14 11:19:39 +0500

    uploading chat file

---
+++ livechat.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>live Chat</title>
+</head>
+<body>
+    <h1>Start a Converstion</h1>
+    <h2>By: Amina Abdul Ghafoor</h2>
+</body>
+</html>
