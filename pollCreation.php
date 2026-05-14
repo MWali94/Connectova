85e20cee1cd16b5caaea3983eb79c0ffa64fc688
Areesha0607 <Areeshasultan.07@gmail.com>
2026-05-14 13:58:36 +0500

    Updated Poll feature

--- pollCreation.php
+++ pollCreation.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Poll Creation</title>
+    <title> Poll Creation </title>
 </head>
 
 <body>
