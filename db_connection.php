36c2a55bbc64e28f4d0a39db261986fe632ca71a
Arooba186 <aroobam.45@icloud.com>
2026-05-20 13:51:59 +0500

    add auth pages

---
+++ db_connection.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "connectova";
+
+$conn =
+mysqli_connect(
+$host,
+$user,
+$password,
+$database
+);
+
+if(!$conn){
+
+die("Connection failed");
+
+}
+
+?>
